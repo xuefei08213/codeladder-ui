b6888d53850b864ec8ec423c05994ef17d6ca48e
jenkins <1067031954@qq.com>
2022-07-06 09:49:10 +0800

    增加认证逻辑

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import router from './router'
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+
+app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import store from '../store';
+
 import CodeladderIndex from '@/components/CodeladderIndex'
 import LoginForm from '@/components/LoginForm'
+import AdminConsole from '@/components/AdminConsole'
 
 const routes = [
 	{
@@ -12,13 +15,30 @@ const routes = [
         path: "/login",
         name: "login",
         component: LoginForm
-        
+    },
+    {
+        path:"/admin",
+        name:"admin",
+        component: AdminConsole,
+        meta: {requiresAuth: true}
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
+  })
+
+router.beforeEach((to, from, next) => {
+    if(to.matched.some(record => record.meta.requiresAuth)) {
+      if (store.getters.isAuthenticated) {
+        next()
+        return
+      }
+      next('/login')
+    } else {
+      next()
+    }
   })
 
 export default router;
